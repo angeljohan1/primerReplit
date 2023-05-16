f8d91a96d12009e51a4bec940e5af54b4d45896e
angeljohan1 <45563687+angeljohan1@users.noreply.github.com>
2023-05-16 23:42:17 +0000

    Se añade productos

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {User} from './user.js'
+import {Product} from './products.js'
 
 const nombre = new User('Angel','Alcazar')
 
 console.log(nombre)
+console.log(new Product("Manzana",2500))
+
